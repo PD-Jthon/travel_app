0dbf1ea326e3ce06e917a892bf2bb1cae84ab3f3
富田卓 <omochimaru1014@gmail.com>
2024-02-22 15:03:36 +0900

    mediaとstaticファイルの変更2

--- server/venv_travel_app/travel_app/config/settings.py
+++ server/venv_travel_app/travel_app/config/settings.py
@@ -2,3 +2,6 @@ from .settings_common import *
 
 STATIC_ROOT = '/usr/share/nginx/html/static_file'
 MEDIA_ROOT = '/usr/share/nginx/html/media_file'
+
+MEDIA_URL = 'media_file/'
+STATIC_URL = 'static_file/'
